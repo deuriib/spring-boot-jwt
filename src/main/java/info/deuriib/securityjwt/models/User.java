1b59d40ab46cf0fce68aa692a59e398edc55c37d
Deuri Vasquez <deuriib@gmail.com>
2023-10-13 02:55:06 -0400

    add user model

---
+++ src/main/java/info/deuriib/securityjwt/models/User.java
@@ -0,0 +1,30 @@
+package info.deuriib.securityjwt.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+    private String username;
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = {@JoinColumn(name = "user_id")},
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private List<Role> roles = new ArrayList<>();
+}
