1f1a380835d1846e864e77ca85b0d282a698fbe3
Deuri Vasquez <deuriib@gmail.com>
2023-10-13 03:13:29 -0400

    add JwtAuthenticationEntryPoint.java

---
+++ src/main/java/info/deuriib/securityjwt/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,19 @@
+package info.deuriib.securityjwt.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+// This annotation is used to mark a java class as a bean so the component-scanning mechanism of spring can pick it up and pull it into the application context.
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+    }
+}
